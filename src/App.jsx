68a61debad956576ff620614419eb1104ab188b5
Wissam <wissamjemal99@gmail.com>
2024-05-27 18:53:40 +0300

    layout and pages done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<Homepage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+    </Route>
+  )
+);
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Hero title="Test Title" subtitle="Tis is the subtitle" />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
+      <RouterProvider router={router} />
     </>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,7 @@ const Navbar = () => {
         <div className="flex h-20 items-center justify-between">
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
             {/* <!-- Logo --> */}
-            <a
-              className="flex flex-shrink-0 items-center mr-4"
-              href="/index.html"
-            >
+            <a className="flex flex-shrink-0 items-center mr-4" href="/">
               <img className="h-10 w-auto" src={logo} alt="React Jobs" />
               <span className="hidden md:block text-white text-2xl font-bold ml-2">
                 React Jobs
@@ -19,19 +16,19 @@ const Navbar = () => {
             <div className="md:ml-auto">
               <div className="flex space-x-2">
                 <a
-                  href="/index.html"
+                  href="/"
                   className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   Home
                 </a>
                 <a
-                  href="/jobs.html"
+                  href="/jobs"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   Jobs
                 </a>
                 <a
-                  href="/add-job.html"
+                  href="/add-job"
                   className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   Add Job
